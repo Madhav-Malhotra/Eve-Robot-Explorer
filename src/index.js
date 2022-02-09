3e0d67052bc66b44b02176efe59e204f435ee811
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-02-09 14:16:23 -0500

    Updated lighting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { 
   Scene, PerspectiveCamera, WebGLRenderer,
-  BoxGeometry, MeshBasicMaterial, Mesh,
+  DirectionalLight, Vector3
 } from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
@@ -18,23 +18,30 @@ const loader = new GLTFLoader(); // Add loader
 document.body.appendChild(renderer.domElement); // Display canvas on page
 
 
-
-
 // ================- ADD OBJECTS ================= //
-const success = (gltf) => scene.add(gltf.scene); // After load finishes 
+const light = new DirectionalLight(0xffffff, 0.9);
+const light2 = new DirectionalLight(0xffffff, 0.9);
+let Eve;
+
+const success = (gltf) => {
+  Eve = gltf.scene;
+  scene.add(Eve);
+  light.target = Eve; light2.target = Eve;
+} // After load finishes 
 const progress = undefined // Report load progress
 const fail = (error) => console.error(error); // If load fails 
 loader.load('assets/Eve.gltf', success, progress, fail);
 
-camera.position.set( 0, 0, 2 );
+camera.position.set( 0, 1, 2.3 ); camera.lookAt(new Vector3(0,0,0));
 
-
-
+scene.add(light); scene.add(light2);
+light.position.set(1,1,1); light2.position.set(-1,-1,1)
 
 // ================== RENDER ================= //
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
+  Eve.rotation.y += 0.01;
 }
 
 render();
