8103ba2e254d1bec1a3c32b797ce50e0ac64517e
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-02-10 09:46:00 -0500

    Finished lighting and added orbits

--- src/index.js
+++ src/index.js
@@ -3,45 +3,64 @@ import {
   DirectionalLight, Vector3
 } from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
 
 // ==================== SETUP ====================== //
-const scene = new Scene(); //Add scene
+const scene = new Scene(); // Add scene
+const loader = new GLTFLoader(); // Add loader
 
-//Add camera. Arguments (field of view in degrees, aspect ratio, closest render distance, furthest render distance)
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new WebGLRenderer(); //Add renderer
+const renderer = new WebGLRenderer(); // Add renderer
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const loader = new GLTFLoader(); // Add loader
+// Add camera. Arguments (field of view in degrees, aspect ratio, closest render distance, furthest render distance)
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Arguments (camera to orbit, canvas element that graphics are rendered to)
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true; // Adds 'weight' to how camera moves
+controls.autoRotate = true; // Automatically starts orbiting
+
+// Call controls.update() after any manual camera transforms
+camera.position.set( 0, 1, 2.3 ); controls.update(); 
+camera.lookAt(new Vector3(0,0,0)); controls.update();
 
 document.body.appendChild(renderer.domElement); // Display canvas on page
 
 
+
+
 // ================- ADD OBJECTS ================= //
 const light = new DirectionalLight(0xffffff, 0.9);
-const light2 = new DirectionalLight(0xffffff, 0.9);
+const light2 = new DirectionalLight(0x08F7FE, 0.9);
+const light3 = new DirectionalLight(0xFE53BB, 0.9);
+const light4 = new DirectionalLight(0xffffff, 0.9);
 let Eve;
 
 const success = (gltf) => {
   Eve = gltf.scene;
   scene.add(Eve);
-  light.target = Eve; light2.target = Eve;
+  light.target = Eve; light2.target = Eve; light3.target = Eve; light4.target = Eve;
 } // After load finishes 
 const progress = undefined // Report load progress
 const fail = (error) => console.error(error); // If load fails 
 loader.load('assets/Eve.gltf', success, progress, fail);
 
-camera.position.set( 0, 1, 2.3 ); camera.lookAt(new Vector3(0,0,0));
+scene.add(light); scene.add(light2); scene.add(light3); scene.add(light4);
+light.position.set(1,2,3); light2.position.set(1,2,-3);
+light3.position.set(-1,2,3); light4.position.set(-1,2,-3);
 
-scene.add(light); scene.add(light2);
-light.position.set(1,1,1); light2.position.set(-1,-1,1)
+
+
 
 // ================== RENDER ================= //
 function render() {
   requestAnimationFrame(render);
+  // Needed BEFORE .render() if controls.enableDamping or controls.autoRotate are set to true 
+  controls.update();  
   renderer.render(scene, camera);
-  Eve.rotation.y += 0.01;
 }
 
 render();
