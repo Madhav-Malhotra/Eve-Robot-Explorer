76fe231816e36dea2a3dc058b45099f61996f6c8
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-02-24 17:01:02 -0500

    Low poly terrain

--- src/script.js
+++ src/script.js
@@ -1,14 +1,15 @@
 // ================ IMPORTS ==================== //
-import './style.css'
+import './style.css';
 import {
     DirectionalLight, Scene, Vector3,
     Object3D, WebGLRenderer, PerspectiveCamera,
-    TextureLoader, PlaneBufferGeometry,
-    MeshStandardMaterial, Mesh, DoubleSide
-} from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+    Color, FogExp2
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 import * as dat from 'dat.gui'
+
 
 
 
@@ -17,10 +18,14 @@ const gui = new dat.GUI(); //Debug
 const canvas = document.querySelector('canvas.webgl');
 const scene = new Scene();
 const loader = new GLTFLoader(); // Add loader
-const textureLoader = new TextureLoader();
+
 
 
 // ======================= VIEW =================== //
+// Add fog
+scene.background = new Color( 0xefd1b5 );
+scene.fog = new FogExp2( 0xefd1b5, 0.0025 );
+
 // Add camera. Arguments (field of view in degrees, aspect ratio, closest render distance, furthest render distance)
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -41,6 +46,8 @@ var temp = new Vector3();
 var goal = new Object3D();
 
 
+
+
 // ================ OBJECTS ================= //
 // Lights
 
@@ -53,10 +60,10 @@ scene.add(light); scene.add(light2); scene.add(light3); scene.add(light4);
 light.position.set(1,2,3); light2.position.set(1,2,-3);
 light3.position.set(-1,2,3); light4.position.set(-1,2,-3);
 
-// Add mesh
+// Add Eve
 let Eve;
 
-const success = (gltf) => {
+const eveLoadSuccess = (gltf) => {
   Eve = gltf.scene;
   Eve.add(goal); scene.add(Eve);
   goal.position.set(-3, 2.1, -2.1);
@@ -66,19 +73,16 @@ const success = (gltf) => {
 const progress = undefined // Report load progress
 const fail = (error) => console.error(error); // If load fails 
 //NOTE: TO ACCESS FILES IN STATIC FOLDER, USE current directory (./)
-loader.load('./Eve.glb', success, progress, fail); 
+loader.load('./Eve.glb', eveLoadSuccess, progress, fail); 
 
-// Add plane
-const height = textureLoader.load("./height.png");
-const planeGeo = new PlaneBufferGeometry(3,3,64,64);
-const planeMaterial = new MeshStandardMaterial({
-  color: "#B7662B",
-  displacementMap: height
-});
-const plane = new Mesh(planeGeo,planeMaterial);
-plane.rotation.x = 1.571; plane.position.y = -1.3; plane.material.side = DoubleSide;
-scene.add(plane);
-gui.add(plane.position, "x"); gui.add(plane.position, "y"); gui.add(plane.position, "z");
+// Terrain
+let terrain;
+const terrainLoadSuccess = (gltf) => {
+  terrain = gltf.scene;
+  terrain.position.y = -30;
+  scene.add(terrain);
+}
+loader.load('./Mars.glb', terrainLoadSuccess, progress, fail); 
 
 
 // ===================== FUNCTIONS ====================== //
@@ -86,18 +90,21 @@ gui.add(plane.position, "x"); gui.add(plane.position, "y"); gui.add(plane.positi
 //Handle mousedown
 let mouseDown = 0;
 orbit.addEventListener("start", () => ++mouseDown ); // mouseDown = 1
-orbit.addEventListener("end", () => --mouseDown); // mouseDown = 
+orbit.addEventListener("end", () => --mouseDown); // mouseDown = 0
+
 // Handle keyboard
 let key = null;
-window.onkeydown = handleKeyboardInput;
+//Keyboard input
 function handleKeyboardInput(e) {
   if (e.key == "ArrowUp") key = "Up";
   if (e.key == "ArrowDown") key = "Down";
   if (e.key == "ArrowLeft") key = "Left";
   if (e.key == "ArrowRight") key = "Right";
-}
+};
+window.onkeydown = handleKeyboardInput;
 window.onkeyup = () => key = null;
 
+// Render
 const render = () => {
   if (!mouseDown) {
     //Update camera position
@@ -107,22 +114,22 @@ const render = () => {
 
   if (key) {
     if (key == "Up") {
-      Eve.translateX(0.03);
-      Eve.translateZ(0.03)
+      Eve.translateX(0.15);
+      Eve.translateZ(0.15);
     }
     if (key == "Down") {
-      Eve.translateX(-0.03);
-      Eve.translateZ(-0.03)
+      Eve.translateX(-0.15);
+      Eve.translateZ(-0.15);
     }
     if (key == "Left") Eve.rotation.y += 0.03;
     if (key == "Right") Eve.rotation.y += -0.03;
   }
 
   // Update Orbital Controls
-  orbit.update()
+  orbit.update();
 
   // Render
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
   // Call tick again on the next frame
   window.requestAnimationFrame(render)
@@ -130,14 +137,14 @@ const render = () => {
 render();
 
 
-
 // ================ RESPONSIVENESS ================== //
-window.addEventListener('resize', () => {
+const onWindowResize = () => {
   // Update camera
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-// Update renderer
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+  // Update renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+}
+window.addEventListener('resize', onWindowResize);
